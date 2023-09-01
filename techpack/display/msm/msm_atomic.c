4d77e62b7366455bbbedb32f247fa10f3e4a1f61
Kazuki H <kazukih0205@gmail.com>
2023-09-01 12:50:35 +0000

    disp: Remove frame stat collector
    Change-Id: I67dd75dc71c7420efc3368c7c054046a4bf5cf77

--- techpack/display/msm/msm_atomic.c
+++ techpack/display/msm/msm_atomic.c
@@ -545,8 +545,6 @@ static void complete_commit(struct msm_commit *c)
 static void _msm_drm_commit_work_cb(struct kthread_work *work)
 {
 	struct msm_commit *commit = NULL;
-	ktime_t start, end;
-	s64 duration;
 	struct pm_qos_request req = {
 		.type = PM_QOS_REQ_AFFINE_CORES,
 		.cpus_affine = BIT(raw_smp_processor_id())
@@ -559,9 +557,6 @@ static void _msm_drm_commit_work_cb(struct kthread_work *work)
 
 	commit = container_of(work, struct msm_commit, commit_work);
 
-	start = ktime_get();
-	frame_stat_collector(0, COMMIT_START_TS);
-
 	/*
 	 * Optimistically assume the current task won't migrate to another CPU
 	 * and restrict the current CPU to shallow idle states so that it won't
@@ -572,10 +567,6 @@ static void _msm_drm_commit_work_cb(struct kthread_work *work)
 	complete_commit(commit);
 	SDE_ATRACE_END("complete_commit");
 	pm_qos_remove_request(&req);
-
-	end = ktime_get();
-	duration = ktime_to_ns(ktime_sub(end, start));
-	frame_stat_collector(duration, COMMIT_END_TS);
 }
 
 static struct msm_commit *commit_init(struct drm_atomic_state *state,
